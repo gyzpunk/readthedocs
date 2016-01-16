78231ab7b4bb63d869a8c3d3ea78cd5998190851
Florent Captier <florent@captier.org>
2016-01-16 14:52:59 +0100

    Use env variable for docker token

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/gyzpunk/readthedocs/trigger/274048bd-f547-4dfb-aaef-939c36382a09/
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/gyzpunk/readthedocs/trigger/${DOCKER_TOKEN}/
